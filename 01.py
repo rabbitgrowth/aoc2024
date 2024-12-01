f49f22a7d1e6e950c487da8a4e9babff002c2588
Nick Yeow <ytknick@gmail.com>
2024-12-01 05:42:16 +0000

    Day 1, part 2

--- 01.py
+++ 01.py
@@ -1,5 +1,10 @@
+from collections import Counter
+
 with open('01.txt') as f:
     nums = [map(int, line.split()) for line in f]
 
-left, right = map(sorted, zip(*nums))
-print(sum(abs(l - r) for l, r in zip(left, right)))
+left, right = list(zip(*nums))
+print(sum(abs(l - r) for l, r in zip(sorted(left), sorted(right))))
+
+count = Counter(right)
+print(sum(l * count[l] for l in left))
