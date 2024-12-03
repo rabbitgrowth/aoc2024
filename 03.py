a9213cc4985add4ee13fc48724afb8bc9e906efb
Nick Yeow <ytknick@gmail.com>
2024-12-03 13:20:22 +0800

    Day 3, part 2

--- 03.py
+++ 03.py
@@ -4,7 +4,16 @@ from math import prod
 with open('03.txt') as f:
     program = f.read()
 
-print(sum(
-    prod(map(int, match.groups()))
-    for match in re.finditer(r'mul\((\d{1,3}),(\d{1,3})\)', program)
-))
+on = True
+total = 0
+
+for match in re.finditer(r"(mul|do(?:n't)?)\((?:(\d{1,3}),(\d{1,3}))?\)", program):
+    func, x, y = match.groups()
+    if func == 'do':
+        on = True
+    elif func == "don't":
+        on = False
+    elif func == 'mul' and on:
+        total += int(x) * int(y)
+
+print(total)
