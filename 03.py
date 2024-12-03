87cfdad50a00ca2f394a555130dfb425dad22a68
Nick Yeow <ytknick@gmail.com>
2024-12-03 17:57:02 +0800

    Rename to enabled

--- 03.py
+++ 03.py
@@ -3,16 +3,16 @@ import re
 with open('03.txt') as f:
     program = f.read()
 
-on = True
+enabled = True
 total = 0
 
 for match in re.finditer(r"(mul|do(?:n't)?)\((?:(\d{1,3}),(\d{1,3}))?\)", program):
     func, x, y = match.groups()
     if func == 'do':
-        on = True
+        enabled = True
     elif func == "don't":
-        on = False
-    elif func == 'mul' and on:
+        enabled = False
+    elif func == 'mul' and enabled:
         total += int(x) * int(y)
 
 print(total)
