5db642424f7532fc26cd1ed81ac56bde6e262179
Nick Yeow <ytknick@gmail.com>
2024-12-19 23:53:50 +0800

    Day 16, part 2 with defaultdict

--- 16.py
+++ 16.py
@@ -1,4 +1,5 @@
-from heapq import heappush, heappop
+import sys
+from collections import defaultdict
 
 wall = set()
 
@@ -12,19 +13,30 @@ with open('16.txt') as f:
             elif char == 'E':
                 end = x, y
 
-queue = [(0, *start, 1, 0)]
-done = set()
+scores = defaultdict(lambda: defaultdict(list))
+scores[0][(*start, 1, 0)]
+
+done = {}
 
 while True:
-    score, x, y, dx, dy = heappop(queue)
-    if (x, y) == end:
-        print(score)
-        break
-    done.add((x, y, dx, dy))
-    for penalty, x, y, dx, dy in [
-        (1,    x + dx, y + dy,  dx,  dy),
-        (1001, x - dy, y + dx, -dy,  dx),
-        (1001, x + dy, y - dx,  dy, -dx),
-    ]:
-        if (x, y) not in wall and (x, y, dx, dy) not in done:
-            heappush(queue, (score + penalty, x, y, dx, dy))
+    score = min(scores)
+    srcs = scores.pop(score)
+    for src, prevs in srcs.items():
+        x, y, dx, dy = src
+        if (x, y) == end:
+            print(score)
+            paths = {end}
+            while prevs:
+                prev = prevs.pop()
+                x, y, dx, dy = prev
+                paths.add((x, y))
+                prevs.extend(done[prev])
+            print(len(paths))
+            sys.exit()
+        for penalty, new_dx, new_dy in [(1, dx, dy), (1001, -dy, dx), (1001, dy, -dx)]:
+            new_x = x + new_dx
+            new_y = y + new_dy
+            if (new_x, new_y) not in wall and (new_x, new_y) not in done:
+                scores[score + penalty][new_x, new_y, new_dx, new_dy].append(src)
+        if src not in done:
+            done[src] = prevs
