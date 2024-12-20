38ff9b9c4aa7e188d1e4059d9b037f640aaee80f
Nick Yeow <ytknick@gmail.com>
2024-12-20 14:46:56 +0800

    Rewrite to use a heap, but it's 4x slower

--- 16.py
+++ 16.py
@@ -1,5 +1,5 @@
 import sys
-from collections import defaultdict
+from heapq import heappush, heappop
 
 wall = set()
 
@@ -9,34 +9,33 @@ with open('16.txt') as f:
             if char == '#':
                 wall.add((x, y))
             elif char == 'S':
-                start = x, y
+                start = (x, y)
             elif char == 'E':
-                end = x, y
+                end = (x, y)
 
-scores = defaultdict(lambda: defaultdict(list))
-scores[0][(*start, 1, 0)]
-
+queue = [(0, *start, 1, 0, None, None, None, None)]
 done = {}
 
 while True:
-    score = min(scores)
-    srcs = scores.pop(score)
-    for src, prevs in srcs.items():
-        x, y, dx, dy = src
-        if (x, y) == end:
-            print(score)
-            paths = {end}
-            while prevs:
-                prev = prevs.pop()
-                x, y, dx, dy = prev
-                paths.add((x, y))
+    score, x, y, dx, dy, prev_x, prev_y, prev_dx, prev_dy = heappop(queue)
+    prevs = [(prev_x, prev_y, prev_dx, prev_dy)]
+    while queue and queue[0][:5] == (score, x, y, dx, dy):
+        prevs.append(heappop(queue)[5:])
+    if (x, y) == end:
+        print(score)
+        paths = set()
+        while prevs:
+            prev = prevs.pop()
+            x, y, dx, dy = prev
+            paths.add((x, y))
+            if prev in done:
                 prevs.extend(done[prev])
-            print(len(paths))
-            sys.exit()
-        for penalty, new_dx, new_dy in [(1, dx, dy), (1001, -dy, dx), (1001, dy, -dx)]:
-            new_x = x + new_dx
-            new_y = y + new_dy
-            if (new_x, new_y) not in wall and (new_x, new_y) not in done:
-                scores[score + penalty][new_x, new_y, new_dx, new_dy].append(src)
-        if src not in done:
-            done[src] = prevs
+        print(len(paths))
+        break
+    for penalty, new_dx, new_dy in [(1, dx, dy), (1001, -dy, dx), (1001, dy, -dx)]:
+        new_x = x + new_dx
+        new_y = y + new_dy
+        if (new_x, new_y) not in wall and (new_x, new_y) not in done:
+            heappush(queue, (score + penalty, new_x, new_y, new_dx, new_dy, x, y, dx, dy))
+    if (x, y, dx, dy) not in done:
+        done[(x, y, dx, dy)] = prevs
