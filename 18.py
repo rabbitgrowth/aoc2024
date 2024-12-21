ca6beacedd9c97dc148178214e425632ed336807
Nick Yeow <ytknick@gmail.com>
2024-12-21 08:58:29 +0000

    Day 18, part 2

--- 18.py
+++ 18.py
@@ -3,28 +3,46 @@ from math import inf
 with open('18.txt') as f:
     blocks = [tuple(map(int, line.split(','))) for line in f]
 
-blocked = set(blocks[:1024])
 start = 0, 0
 end = 70, 70
 moves = [(0, -1), (0, 1), (-1, 0), (1, 0)]
 
-unvisited = {
-    (x, y): 0 if (x, y) == start else inf
-    for x in range(end[0] + 1)
-    for y in range(end[1] + 1)
-    if (x, y) not in blocked
-}
+def search(blocks):
+    unvisited = {
+        (x, y): 0 if (x, y) == start else inf
+        for x in range(end[0] + 1)
+        for y in range(end[1] + 1)
+        if (x, y) not in set(blocks)
+    }
+    while unvisited:
+        x, y = min(unvisited, key=unvisited.get)
+        if (x, y) == end:
+            return dist
+        dist = unvisited.pop((x, y))
+        for dx, dy in moves:
+            new_dist = dist + 1
+            new_x = x + dx
+            new_y = y + dy
+            if (new_x, new_y) in unvisited:
+                if new_dist < unvisited[new_x, new_y]:
+                    unvisited[new_x, new_y] = new_dist
+    return inf
 
-while unvisited:
-    x, y = min(unvisited, key=unvisited.get)
-    if (x, y) == end:
-        print(dist)
+print(search(blocks[:1024]))
+
+idxs = list(range(len(blocks)))
+
+while True:
+    mid = len(idxs) // 2
+    idx = idxs[mid]
+    prev = search(blocks[:idx])
+    curr = search(blocks[:idx+1])
+    if prev < inf and curr < inf:
+        idxs = idxs[mid+1:]
+    elif prev == inf and curr == inf:
+        idxs = idxs[:mid]
+    elif prev < inf and curr == inf:
+        print(','.join(map(str, blocks[idx])))
         break
-    dist = unvisited.pop((x, y))
-    for dx, dy in moves:
-        new_dist = dist + 1
-        new_x = x + dx
-        new_y = y + dy
-        if (new_x, new_y) in unvisited:
-            if new_dist < unvisited[new_x, new_y]:
-                unvisited[new_x, new_y] = new_dist
+    else:
+        assert False
